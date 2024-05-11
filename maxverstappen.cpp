cecf86f4aecdd358d9e19254060fcc47230e6690
PalleSoffritte <153986623+PalleSoffritte@users.noreply.github.com>
2024-05-11 09:36:24 +0000

    asdasd

---
+++ maxverstappen.cpp
@@ -0,0 +1,37 @@
+// NOTE: it is recommended to use this even if you don't understand the following code.
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // uncomment the two following lines if you want to read/write from files
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+
+    int N;
+    cin >> N;
+    int x;
+    int y;
+    vector<int> P(N);
+    for (int i = 0; i < N; ++i)
+        cin >> P[i];
+        if (P[0]>P[N-1])
+        {
+            int 
+        }
+        
+    int K = 0;
+    
+
+
+    
+
+
+    cout << K << endl;
+
+    return 0;
+}
